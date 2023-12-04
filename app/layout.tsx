d9d5e7967e02871aadc3291e5ad438bd8d4c6fc8
unknown <ds4950@naver.com>
2023-12-04 17:54:11 +0900

    fav

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: '오늘의 운세',
   description: '운세',
+  icons: {
+    icon: "/favicon.ico",
+  },
 }
 
 export default function RootLayout({
